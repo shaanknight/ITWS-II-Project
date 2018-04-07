2d17824797bc0d66b1c0e724ff68b8e4f955295b
Tanmay <tanmaykumarsinha18@gmail.com>
2018-04-08 02:12:45 +0530

    Added basic templates and Database structures.

--- swagoverflow.py
+++ swagoverflow.py
@@ -1,7 +1,17 @@
 from flask import Flask,redirect,url_for,render_template
+import sqlite3
 
 app=Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-	return render_template('base.html')
+	return render_template('index.html')
+
+@app.route('/courses/')
+@app.route('/courses/<coursename>')
+def course(coursename=None):
+	conn=sqlite3.connect('questions.db')
+	c=conn.cursor()
+	c.execute('select questions from courses where name=?', (coursename,))
+	ques=c.fetchall()
+	return render_template("coursemain.html", coursename=coursename, questions=ques)
